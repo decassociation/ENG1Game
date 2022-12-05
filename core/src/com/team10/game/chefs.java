37f6d743a99485475f4ed3acced133bc9dbc0447
LucyWalsh2276 <lw2276@york.ac.uk>
2022-12-05 14:03:28 +0000

    Altered chefs class

--- core/src/com/team10/game/chefs.java
+++ core/src/com/team10/game/chefs.java
@@ -1,25 +1,52 @@
 package com.team10.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Rectangle;
+
 public class chefs {
+    private Rectangle chef;
+    private Integer xPos;
+    private Integer yPos;
+    private Boolean isWorking;
 
-    private static Integer activeChef;
 
+    public chefs(Integer x, Integer y) {
+        xPos = x;
+        yPos = y;
+        isWorking = false;
 
-
-    public static Integer getActiveChef(){
-        return activeChef;
+        //Chef instantiation
+        chef = new Rectangle();
+        chef.x = x;
+        chef.y = y;
+        chef.width = 120;
+        chef.height = 60;
     }
-    public void setActiveChef(Integer chefNo){
-        activeChef = chefNo;
+
+    public Integer getX() {
+        return xPos;
+    }
+    public Integer getY() {
+        return yPos;
+    }
+    public void setX(Integer xValue) {
+        //chef will only move if the chef is not currently working at a station
+        if (!isWorking) {
+            chef.x += xValue * Gdx.graphics.getDeltaTime();
+            xPos += xValue;
+        }
+    }
+    public void setY(Integer yValue) {
+        if (!isWorking) {
+            chef.y += yValue * Gdx.graphics.getDeltaTime();
+            yPos += yValue;
+        }
     }
 
-
-
-
-
-
-
-
-
-
-
+    public boolean isWorking() {
+        return isWorking;
+    }
+    public void updateWorking() {
+        isWorking = !isWorking;
+    }
 }

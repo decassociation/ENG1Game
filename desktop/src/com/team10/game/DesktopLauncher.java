eabfff251a28b1b4420c2b1fa239e19a51372682
popESF <pf693@york.ac.uk>
2022-12-20 20:08:01 +0000

    Implemented borderless window settings, now the game is auto full screen, pres ESC to exit

--- desktop/src/com/team10/game/DesktopLauncher.java
+++ desktop/src/com/team10/game/DesktopLauncher.java
@@ -1,6 +1,5 @@
 package com.team10.game;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
@@ -8,7 +7,7 @@ public class DesktopLauncher {
 		public static void main (String[] arg) {
 			Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 			config.setTitle("Piazza Panic");
-			config.setWindowedMode(1366, 768);
+			config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
 			config.useVsync(true);
 			config.setForegroundFPS(60);
 			new Lwjgl3Application(new Eng1Game(), config);

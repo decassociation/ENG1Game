7c56a4fe3bb4e051bb9ddceaf742efbc9c515b42
TitaniumTurbine <63096937+TitaniumTurbine@users.noreply.github.com>
2022-12-27 19:54:03 +0000

    refactored chef control to its own class

---
+++ core/src/com/team10/game/ChefController.java
@@ -0,0 +1,106 @@
+package com.team10.game;
+
+import java.util.ArrayList;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class ChefController {
+    private int chefCount;
+    private int currentChef;
+    private ArrayList<Chef> chefs;
+    private ArrayList<Texture> chefTextures;
+
+    public ChefController(){
+        chefCount = 2;
+
+        chefs = new ArrayList<Chef>();
+        chefs.add(new Chef(500,500));
+        chefs.add(new Chef(873,400));
+
+        currentChef = 0;
+
+        chefTextures = new ArrayList<Texture>();
+        chefTextures.add(new Texture(Gdx.files.internal("chef12.png"))); //chef1 takes chef1(version 2) texture
+        chefTextures.add(new Texture(Gdx.files.internal("chef22.png"))); //chef2 takes chef2(version 2) texture
+    }
+
+    public void swapChef(){
+        if (Gdx.input.isKeyJustPressed(Input.Keys.TAB)) {
+            if (currentChef < chefCount - 1) currentChef += 1;
+            else currentChef = 0;
+        }
+
+        for (int i = 0; i < 9; i++) {   // cycle through each number 1-9 to check if that number key has been pressed
+            if (Gdx.input.isKeyJustPressed(i + 8)) {    // integer code for number keys 1-9 is 8-16 so add 8 to i to get the key code
+                if (chefCount >= i + 1) currentChef = i;
+            }
+        }
+        
+
+    }
+
+    public void chefMovement() {
+        //activeChef = Chef.getActiveChef();
+        Integer up = Input.Keys.W; //This way we can have mappable keys for the controls.
+        Integer left = Input.Keys.A;
+        Integer down = Input.Keys.S;
+        Integer right = Input.Keys.D;
+
+		/*
+		This section checks if the chef is in a "good" place, i.e. not in a wall.
+		If the chef is in a wall, denoted by them being in the given boundaries shown below,
+		they are moved to be not in that wall.
+
+		This is an initial and somewhat brutal method, but for now it will stick
+
+		If later a better option is found, then we can ammend this.
+
+		 */
+        if(chefs.get(currentChef).getX() <= 0){  //left side of map
+            chefs.get(currentChef).setX(10);
+        }
+        if(chefs.get(currentChef).getY() <= 0){  //bottom of the map
+            chefs.get(currentChef).setY(10);
+        }
+        if(chefs.get(currentChef).getX() >= -100 && chefs.get(currentChef).getX() < 873 && chefs.get(currentChef).getY() > 650){  //bottom of counter
+            chefs.get(currentChef).setY(-10);
+        }
+        if(chefs.get(currentChef).getY() < 840 && chefs.get(currentChef).getY() > 560 && chefs.get(currentChef).getX() > 740){  //side of Burger area
+            chefs.get(currentChef).setX(-10);
+        }
+        if(chefs.get(currentChef).getX() >= 770 && chefs.get(currentChef).getX() < 1850 && chefs.get(currentChef).getY() > 540){  //bottom of Burger area
+            chefs.get(currentChef).setY(-10);
+        }
+        if(chefs.get(currentChef).getY() < 830 && chefs.get(currentChef).getY() > 0 && chefs.get(currentChef).getX() > 1500){  //left side of Salad area
+            chefs.get(currentChef).setX(-10);
+        }
+        if(chefs.get(currentChef).getX() >= 0 && chefs.get(currentChef).getX() < 1850 && chefs.get(currentChef).getY() < 230){  //top of lower counter
+            chefs.get(currentChef).setY(10);
+        }
+
+        if (Gdx.input.isKeyPressed(right)) chefs.get(currentChef).setX(10);
+        if (Gdx.input.isKeyPressed(left)) chefs.get(currentChef).setX(-10);
+        if (Gdx.input.isKeyPressed(down)) chefs.get(currentChef).setY(-10);
+        if (Gdx.input.isKeyPressed(up)) chefs.get(currentChef).setY(10);
+
+		/*
+		This whole section just checks which chef is activated and uses their section, and moves them by 250 units each loop.
+		If one were to add more Chef, then they could use an elseif structure to check between Chef,
+		but in this case, since there are only two option, an if/ else is sufficient
+		*/
+    }
+
+    public void update(){
+        swapChef();
+        chefMovement();
+    }
+
+    public void drawChefs(SpriteBatch batch){
+        for (int i = 0; i < chefCount; i++) {
+            batch.draw(chefTextures.get(i), chefs.get(i).getX(), chefs.get(i).getY());
+        }
+    }
+}
--- core/src/com/team10/game/GameScreen.java
+++ core/src/com/team10/game/GameScreen.java
@@ -12,12 +12,7 @@ public class GameScreen implements Screen {
     private Boolean paused = false;   //Useful for when we want to implement a pause function
     private Eng1Game game;
 
-    private Chef chef1;
-    private Chef chef2;
-    private Chef activeChef;
-
-    private Texture chef1img;  //Declares chef1 to be used later as a texture
-    private Texture chef2img;  //Declares chef2 to be used later as a texture
+    private ChefController chefController;
     private Texture img;
 
     private Boolean mainmenu;
@@ -33,23 +28,9 @@ public class GameScreen implements Screen {
         camera.setToOrtho(false, 1850, 1040);
         img = new Texture("layout.png"); //img is the background image
 
-        chef1img = new Texture(Gdx.files.internal("chef12.png")); //chef1 takes chef1(version 2) texture
-
-        chef2img = new Texture(Gdx.files.internal("chef22.png")); //chef2 takes chef2(version 2) texture
+        chefController = new ChefController();
 
         mainmenu = false;
-        chef1 = new Chef(500,500);
-        //chef1.x = 500;
-        //chef1.y = 500;
-        //chef1.width = 120;
-        //chef1.height = 60;
-        activeChef = chef1;
-
-        chef2 = new Chef(873,400);
-        //chef2.x = 873;
-        //chef2.y = 400;
-        //chef2.width = 120;
-        //chef2.height = 60;
 
     }
 
@@ -59,77 +40,12 @@ public class GameScreen implements Screen {
     }
 
 
-
-
-    public void swapChef(){
-        if (Gdx.input.isKeyJustPressed(Input.Keys.TAB)) {  //For us using two Chef, I have used tab as the key to switch, however with multiple Chef, numbers could be used in place of tab, using an elseif structure.
-            if(activeChef == chef1){
-                activeChef = chef2;
-            } else {
-                activeChef = chef1;
-            }
-        }
-
-    }
-
-    public void chefMovement() {
-        //activeChef = Chef.getActiveChef();
-        Integer up = Input.Keys.W; //This way we can have mappable keys for the controls.
-        Integer left = Input.Keys.A;
-        Integer down = Input.Keys.S;
-        Integer right = Input.Keys.D;
-
-		/*
-		This section checks if the chef is in a "good" place, i.e. not in a wall.
-		If the chef is in a wall, denoted by them being in the given boundaries shown below,
-		they are moved to be not in that wall.
-
-		This is an initial and somewhat brutal method, but for now it will stick
-
-		If later a better option is found, then we can ammend this.
-
-		 */
-        if(activeChef.getX() <= 0){  //left side of map
-            activeChef.setX(10);
-        }
-        if(activeChef.getY() <= 0){  //bottom of the map
-            activeChef.setY(10);
-        }
-        if(activeChef.getX() >= -100 && activeChef.getX() < 873 && activeChef.getY() > 650){  //bottom of counter
-            activeChef.setY(-10);
-        }
-        if(activeChef.getY() < 840 && activeChef.getY() > 560 && activeChef.getX() > 740){  //side of Burger area
-            activeChef.setX(-10);
-        }
-        if(activeChef.getX() >= 770 && activeChef.getX() < 1850 && activeChef.getY() > 540){  //bottom of Burger area
-            activeChef.setY(-10);
-        }
-        if(activeChef.getY() < 830 && activeChef.getY() > 0 && activeChef.getX() > 1500){  //left side of Salad area
-            activeChef.setX(-10);
-        }
-        if(activeChef.getX() >= 0 && activeChef.getX() < 1850 && activeChef.getY() < 230){  //top of lower counter
-            activeChef.setY(10);
-        }
-
-        if (Gdx.input.isKeyPressed(right)) activeChef.setX(10);
-        if (Gdx.input.isKeyPressed(left)) activeChef.setX(-10);
-        if (Gdx.input.isKeyPressed(down)) activeChef.setY(-10);
-        if (Gdx.input.isKeyPressed(up)) activeChef.setY(10);
-
-		/*
-		This whole section just checks which chef is activated and uses their section, and moves them by 250 units each loop.
-		If one were to add more Chef, then they could use an elseif structure to check between Chef,
-		but in this case, since there are only two option, an if/ else is sufficient
-		*/
-    }
-
     public void drawStuff(){
         ScreenUtils.clear(1, 0, 0, 1);
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
         batch.draw(img, 0, 0);
-        batch.draw(chef1img, chef1.getX(), chef1.getY());
-        batch.draw(chef2img, chef2.getX(), chef2.getY());
+        chefController.drawChefs(batch);
         batch.end();
     }
 
@@ -141,8 +57,7 @@ public class GameScreen implements Screen {
                 e.printStackTrace();
             }
         }else if(!mainmenu){
-            swapChef(); //checks if the chef needs to be changed
-            chefMovement();  //does the movement for the Chef
+            chefController.update();
             drawStuff(); //Does the actual drawing of the game
 
 

c4183511f15634b0596c9b8b51258d8a154f67fe
OwenLister-GitHub <owenl9.ol@gmail.com>
2023-01-04 19:04:03 +0000

    Implemented the tilemap as the map for the game. Further work needed.

--- core/src/com/team10/game/GameScreen.java
+++ core/src/com/team10/game/GameScreen.java
@@ -6,6 +6,8 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.*;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class GameScreen extends Eng1Screen {
@@ -15,11 +17,21 @@ public class GameScreen extends Eng1Screen {
     private Texture img;
     FileManager fileManager = new FileManager();
 
+    private OrthogonalTiledMapRenderer renderer;
+
+    private TiledMap tileMap;
+
+    float unitScale;
+
     private Boolean mainmenu;
     public GameScreen(Eng1Game game) {
         super(game);
 
-        camera.setToOrtho(false, 1850, 1040);
+        tileMap = new TmxMapLoader().load("pp_assessment_1_tilemap_V2.tmx");
+        unitScale = 1 / 16f;
+        renderer = new OrthogonalTiledMapRenderer(tileMap, unitScale);
+
+        camera.setToOrtho(false, 35, 30);
         img = new Texture("layout.png"); //img is the background image
 
         chefController = new ChefController();
@@ -38,7 +50,7 @@ public class GameScreen extends Eng1Screen {
         ScreenUtils.clear(1, 0, 0, 1);
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
-        batch.draw(img, 0, 0);
+        // batch.draw(img, 0, 0);
         chefController.drawChefs(batch);
         batch.end();
     }
@@ -74,12 +86,16 @@ public class GameScreen extends Eng1Screen {
     public void render (float delta) { //The constantly looping function
         gameLogic(); //Extracts what is being done incase I need to do other none-game logic related things in the render function. This may be changed in future if it turns out nothing else needs doing.
 
-
+        camera.update();
+        renderer.setView(camera);
+        renderer.render();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
         img.dispose();
+        renderer.dispose();
+        tileMap.dispose();
     }
 }

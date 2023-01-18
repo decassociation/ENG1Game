d73c89c32e7d6aee3d9381697afac2c31c8df58f
TitaniumTurbine <63096937+TitaniumTurbine@users.noreply.github.com>
2023-01-18 11:19:32 +0000

    fixed memory leak

---
+++ desktop/bin/main/food_and_crockery.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="food_and_crockery" tilewidth="16" tileheight="16" tilecount="448" columns="32">
+ <image source="props_tileset.png" trans="adbad0" width="512" height="224"/>
+</tileset>

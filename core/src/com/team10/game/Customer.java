14690dcdf320df6c7782c2adcc93d9733d3f7214
TitaniumTurbine <63096937+TitaniumTurbine@users.noreply.github.com>
2023-01-19 15:57:59 +0000

    customers queuing and display request

--- core/src/com/team10/game/Customer.java
+++ core/src/com/team10/game/Customer.java
@@ -3,9 +3,13 @@ package com.team10.game;
 public class Customer {
     
     boolean served;
+    String recipe;
+    float yPos;
 
-    public Customer(){
+    public Customer(String recipe){
         served = false;
+        this.recipe = recipe;
+        yPos = -5;
         
     }
 }
--- core/src/com/team10/game/CustomerController.java
+++ core/src/com/team10/game/CustomerController.java
@@ -3,6 +3,7 @@ package com.team10.game;
 
 import java.time.Clock;
 import java.util.ArrayList;
+import java.util.Random;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Camera;
@@ -10,6 +11,7 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeType.Bitmap;
 
 public class CustomerController {
     
@@ -20,6 +22,12 @@ public class CustomerController {
     int customerCount;
     Texture texture = new Texture(Gdx.files.internal("Customer.png"));
     BitmapFont font = new BitmapFont();
+    BitmapFont font2 = new BitmapFont();
+
+    // array of names of the different possible recipes
+    String recipes[] = {"burger", "salad"};
+
+    Random generator = new Random();
 
 
     public CustomerController(){
@@ -27,6 +35,12 @@ public class CustomerController {
         startTime = timer.millis();
         timeOfLastCustomer = 0;
         customers = new ArrayList<Customer>();
+
+        font.setColor(Color.BLACK);
+        font.getData().setScale(0.1f, 0.15f);
+
+        font2.setColor(Color.BLACK);
+        font2.getData().setScale(0.1f, 0.1f);
 
     }
 
@@ -36,8 +50,7 @@ public class CustomerController {
 
     public void update(SpriteBatch batch, Camera camera){
         // draw timer
-        font.setColor(Color.BLACK);
-        font.getData().setScale(0.1f, 0.15f);
+        
         font.draw(batch, Long.toString(getCurrentTime()/1000), 1, 30);
 
         // create new customers
@@ -49,16 +62,26 @@ public class CustomerController {
 
     public void drawCustomers(SpriteBatch batch){
         for (int i = 0; i < customers.size(); i++) {
-            batch.draw(texture, 30, i * 5, 3.0f, 3.0f);
+            batch.draw(texture, 5, customers.get(i).yPos, 2.0f, 2.0f);
+            if(customers.get(i).yPos < 10 - (3*i)){
+                customers.get(i).yPos += 0.2f;
+            }
+            if(i == 0 && customers.get(0).yPos >= 10 - (3*i)){
+                font2.draw(batch, customers.get(i).recipe, 0, customers.get(i).yPos + 1.75f);
+            }
         }
     }
 
     private void createCustomers_scenario(int totalCustomers){
-        if (getCurrentTime()-timeOfLastCustomer >= 10000 && customerCount < totalCustomers){
-            customers.add(new Customer());
+        if (customers.size() == 0 || getCurrentTime()-timeOfLastCustomer >= 10000 && customerCount < totalCustomers){
+            customers.add(new Customer(recipes[generator.nextInt(recipes.length)]));
             customerCount += 1;
             timeOfLastCustomer = getCurrentTime();
             System.out.println(customers.toString());
+        }
+
+        if (customers.size() >= 5 && getCurrentTime()-timeOfLastCustomer >= 10000){
+            customers.remove(0);
         }
         
     }

ac9ef4e424f0b3a19c7548b6364ac19a9dcead4f
poppy <pf693@york.ac.uk>
2023-01-31 20:13:44 +0000

    New tutorial with screens created

--- core/src/com/team10/menus/TutorialScreen.java
+++ core/src/com/team10/menus/TutorialScreen.java
@@ -1,5 +1,6 @@
 package com.team10.menus;
 
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.Gdx;
@@ -7,17 +8,15 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.Screen;
 import com.team10.game.Eng1Game;
 
-
-
-
 public class TutorialScreen implements Screen {
     public SpriteBatch batch;
     public Texture TutorialImg;
     private final ChangeScreenButton mainMenuButton;
+    private int currentImage = 1;
 
     public TutorialScreen(Eng1Game game, Camera camera) {
         this.batch = game.batch;
-        TutorialImg = new Texture(Gdx.files.internal("Tutorial.png"));
+        TutorialImg = new Texture(Gdx.files.internal("Tutorial1.png"));
         mainMenuButton = new ChangeScreenButton(20, 20, 100, 50, "Main Menu", batch, camera, game, "mainMenu");
     }
 
@@ -29,11 +28,24 @@ public class TutorialScreen implements Screen {
     @Override
     public void render(float delta) {
         // called every frame, use this to update and draw the screen
-            batch.begin();
-            batch.draw(TutorialImg, 0, 0,800, 480);
-            mainMenuButton.update();
-            batch.end();
+        if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)) {
+            if (currentImage < 3) {
+                currentImage++;
+                TutorialImg = new Texture(Gdx.files.internal("Tutorial" + currentImage + ".png"));
+            }
         }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT)) {
+            if (currentImage > 1) {
+                currentImage--;
+                TutorialImg = new Texture(Gdx.files.internal("Tutorial" + currentImage + ".png"));
+            }
+        }
+        batch.begin();
+        batch.draw(TutorialImg, 0, 0, 800, 480);
+        mainMenuButton.update();
+        batch.end();
+
+    }
 
     @Override
     public void resize(int width, int height) {
